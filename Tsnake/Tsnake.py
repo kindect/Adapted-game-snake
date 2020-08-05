7170267e8fa05a8da0c311ba489558fa9344faa3
kindect <kindect@163.com>
2020-08-05 18:13:21 +0800

    PEP Grammar fix

--- Tsnake/Tsnake.py
+++ Tsnake/Tsnake.py
@@ -24,42 +24,43 @@ MAX_Y = WINDOW_WIDTH // BLOCK_SIZE
 background_image = pygame.image.load("pics/background.png")
 
 # BLOCK_SIZE*BLOCK_SIZE pixel
-body_image_1=pygame.image.load('pics/body/1.png')
-body_image_2=pygame.image.load('pics/body/2.png')
-body_image_3=pygame.image.load('pics/body/3.png')
-body_image_12=pygame.image.load('pics/body/12.png')
-body_image_13=pygame.image.load('pics/body/13.png')
-body_image_23=pygame.image.load('pics/body/23.png')
+body_image_1 = pygame.image.load('pics/body/1.png')
+body_image_2 = pygame.image.load('pics/body/2.png')
+body_image_3 = pygame.image.load('pics/body/3.png')
+body_image_12 = pygame.image.load('pics/body/12.png')
+body_image_13 = pygame.image.load('pics/body/13.png')
+body_image_23 = pygame.image.load('pics/body/23.png')
 
-head_image_0=pygame.image.load('pics/head/0.png')
-head_image_1=pygame.image.load('pics/head/1.png')
-head_image_2=pygame.image.load('pics/head/2.png')
-head_image_3=pygame.image.load('pics/head/3.png')
+head_image_0 = pygame.image.load('pics/head/0.png')
+head_image_1 = pygame.image.load('pics/head/1.png')
+head_image_2 = pygame.image.load('pics/head/2.png')
+head_image_3 = pygame.image.load('pics/head/3.png')
 
-food_image=pygame.image.load('pics/food.png')
+food_image = pygame.image.load('pics/food.png')
 
-poison_image=pygame.image.load('pics/poison.png')
+poison_image = pygame.image.load('pics/poison.png')
+
 
 def pattern(op, direction):
-    dirp = 3 - direction
-    return min(op, dirp) * 10 + max(op, dirp)
+    direction_p = 3 - direction
+    return min(op, direction_p) * 10 + max(op, direction_p)
 
 
 # noinspection PyGlobalUndefined
 class Snake:
     def __init__(self, body=None, head=None, directions=None, head_direction=None):
-        self.run=False
-        self.pushtask=threading.Thread(target=self.push)
-        if self.pushtask.is_alive():
-            self.pushtask.join()
+        self.run = False
+        self.push_task = threading.Thread(target=self.push)
+        if self.push_task.is_alive():
+            self.push_task.join()
         if directions is None:
             directions = [3] * 4
         if head is None:
-            head=(0,4)
+            head = (0, 4)
         if body is None:
             body = [(0, 0), (0, 1), (0, 2), (0, 3)]
         if head_direction is None:
-            head_direction=3
+            head_direction = 3
         self.body = body
         self.head = head
         self.directions = directions
@@ -68,20 +69,19 @@ class Snake:
         self.run = True
 
     def print(self):
-        exec("screen.blit(head_image_" + str(self.head_direction) + ",(self.head[1]*BLOCK_SIZE,self.head[0]*BLOCK_SIZE))")
+        exec("screen.blit(head_image_" + str(
+            self.head_direction) + ",(self.head[1]*BLOCK_SIZE,self.head[0]*BLOCK_SIZE))")
         for i in range(len(self.body)):
-            exec("screen.blit(body_image_" + str(self.directions[i]) + ",(self.body[i][1]*BLOCK_SIZE,self.body[i][0]*BLOCK_SIZE))")
+            exec("screen.blit(body_image_" + str(
+                self.directions[i]) + ",(self.body[i][1]*BLOCK_SIZE,self.body[i][0]*BLOCK_SIZE))")
+
     def move(self, direction):
         if direction + self.head_direction == 3:
             return
         self.body.append(self.head)
-        self.directions.append(pattern(direction,self.head_direction))
-        next=[(0,-1),(-1,0),(1,0),(0,1)]
-        tmp=(self.head[0]+next[direction][0],self.head[1]+next[direction][1])
-        if(tmp[0]>=MAX_X or tmp[0]<0 or tmp[1]>=MAX_Y or tmp[1]<0 or tmp in self.body or tmp in poisons):
-            print('[INFO]: dead')
-        nextp = [(0, -1), (-1, 0), (1, 0), (0, 1)]
-        tmp = (self.head[0] + nextp[direction][0], self.head[1] + nextp[direction][1])
+        self.directions.append(pattern(direction, self.head_direction))
+        next_p = [(0, -1), (-1, 0), (1, 0), (0, 1)]
+        tmp = (self.head[0] + next_p[direction][0], self.head[1] + next_p[direction][1])
         if tmp[0] >= MAX_X or tmp[0] < 0 or tmp[1] >= MAX_Y or tmp[1] < 0 or tmp in self.body or tmp in poisons:
             print("[INFO]: dead")
             self.run = False
@@ -89,25 +89,27 @@ class Snake:
             return
         self.head = tmp
         self.head_direction = direction
-        flag = False
         global foods
-        if(self.head in foods):
-            if(len(foods)<=3):
-                foods[foods.index(self.head)]=generate()
+        if self.head in foods:
+            if len(foods) <= 3:
+                foods[foods.index(self.head)] = generate()
             else:
-                del(foods[foods.index(self.head)])
+                del (foods[foods.index(self.head)])
         else:
             del (self.directions[0])
             del (self.body[0])
+
     def push(self):
         while self.run:
             sleep(DELAY)
             self.move(self.head_direction)
 
+
 class Game:
     def __init__(self):
-        global user,foods,poisons
-        poisons=[]
+        # noinspection PyGlobalUndefined
+        global user, foods, poisons
+        poisons = []
         screen.blit(background_image, (0, 0))
         user = Snake()
         threading.Thread(target=user.push).start()
@@ -117,39 +119,44 @@ class Game:
             foods.append(generate())
         for i in range(POISON_AMOUNT):
             poisons.append(generate())
-    def print(self):
+
+    @staticmethod
+    def print():
         user.print()
         for food in foods:
-            screen.blit(food_image,(food[1]*BLOCK_SIZE,food[0]*BLOCK_SIZE))
+            screen.blit(food_image, (food[1] * BLOCK_SIZE, food[0] * BLOCK_SIZE))
         for poison in poisons:
-            screen.blit(poison_image,(poison[1]*BLOCK_SIZE,poison[0]*BLOCK_SIZE))
-        screen.blit(font.render(str((len(user.body)+1)*5),False,(255,200,10)),(10,10))
+            screen.blit(poison_image, (poison[1] * BLOCK_SIZE, poison[0] * BLOCK_SIZE))
+        screen.blit(font.render(str((len(user.body) + 1) * 5), False, (255, 200, 10)), (10, 10))
+
     def mainloop(self):
-        while(True):
-            key_list=pygame.key.get_pressed()
+        while True:
+            key_list = pygame.key.get_pressed()
             for event in pygame.event.get():
                 # event queue
-                if(event.type==QUIT):
+                if event.type == QUIT:
                     print('[INFO]: exit requested')
                     pygame.quit()
-                if(event.type==KEYDOWN):
-                    if(event.key==K_LEFT or key_list[K_LEFT]):
+                if event.type == KEYDOWN:
+                    if event.key == K_LEFT or key_list[K_LEFT]:
                         print('[INFO]: left pressed')
                         user.move(0)
-                    if(event.key==K_RIGHT or key_list[K_RIGHT]):
+                    if event.key == K_RIGHT or key_list[K_RIGHT]:
                         print('[INFO]: right pressed')
                         user.move(3)
-                    if(event.key==K_UP or key_list[K_UP]):
+                    if event.key == K_UP or key_list[K_UP]:
                         print('[INFO]: up pressed')
                         user.move(1)
-                    if(event.key==K_DOWN or key_list[K_DOWN]):
+                    if event.key == K_DOWN or key_list[K_DOWN]:
                         print('[INFO]: down pressed')
                         user.move(2)
-            screen.fill((255,255,255))
-            screen.blit(background_image,(0,0))
+            screen.fill((255, 255, 255))
+            screen.blit(background_image, (0, 0))
             self.print()
             pygame.time.Clock().tick(60)
             pygame.display.update()
+
+
 def generate():
     tmp = (randint(0, MAX_X - 1), randint(0, MAX_Y - 1))
     if tmp in user.body or tmp == user.head or tmp in foods or tmp in poisons:
@@ -162,9 +169,9 @@ if __name__ == "__main__":
         print("[Error]: Bad WINDOW_WIDTH WINDOW_HEIGHT")
     pygame.init()
     pygame.font.init()
-    screen=pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT),0,32)
-    font =  pygame.font.SysFont('microsoft Yahei',30)
-    pygame.display.set_caption('Tsnake')
-    ngame = Game()
-    maintask = threading.Thread(target=ngame.mainloop)
-    maintask.run()
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
+    font = pygame.font.SysFont('microsoft Yahei', 30)
+    pygame.display.set_caption('T_snake')
+    game_p = Game()
+    main_task = threading.Thread(target=game_p.mainloop)
+    main_task.run()
